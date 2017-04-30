8486fbbae605ed2664b02927eec224f103440f23
Wangmoaza <haeun0512@hanmail.net>
2017-05-01 02:04:53 +0900

    print error

--- tsh.c
+++ tsh.c
@@ -197,6 +197,7 @@ void eval(char *cmdline)
     /* if not built-in cmd, fork child process & run the job in child */
     if (!builtin_cmd(argv))
     {
+	printf("should not reach here\n"); //FIXME delete this 
         /*
          * parent must use sigprocmask to block SIGCHLD signal before it forks the child
          * and unblock these signals, again using sigprocmask
@@ -340,8 +341,8 @@ void do_bgfg(char **argv)
 void waitfg(pid_t pid)
 {
     // use a busy loop around the sleep function
-    while (pid == fgpid(jobs))
-        sleep(0);
+    //while (pid == fgpid(jobs))
+    //    sleep(0);
     return;
 }
 
